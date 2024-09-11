2ea575505997c8c06666815e4665c895e9413b12
taewoo-dev <twin9774@gmail.com>
2024-09-11 11:58:33 +0900

    feat: implement prefetch

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,5 +1,16 @@
+from django.core.paginator import Page, Paginator
+from django.db.models import QuerySet
+
 from tabom.models import Article
 
 
 def get_an_article(article_id: int) -> Article:
     return Article.objects.get(id=article_id)
+
+
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id").prefetch_related("like_set")[offset : offset + limit]
+
+
+# def get_article_page(page: int, limit: int) -> Page[Article]:
+#     return Paginator(Article.objects.order_by("-id"), limit).page(page)
--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -1,7 +1,9 @@
+from django.db import connection
 from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
 
-from tabom.models import Article
-from tabom.services.article_service import get_an_article
+from tabom.models import Article, Like, User
+from tabom.services.article_service import get_an_article, get_article_list
 
 
 class TestArticleService(TestCase):
@@ -24,3 +26,31 @@ class TestArticleService(TestCase):
         # Expect
         with self.assertRaises(Article.DoesNotExist):
             get_an_article(invalid_article_id)
+
+    def test_get_article_list_should_prefetch_likes(self) -> None:
+        # Given
+        user = User.objects.create(name="test_user")
+        # test라 for문, 한방쿼리는 bulk_create
+        articles = [Article.objects.create(title=f"test_article_{i}") for i in range(1, 21)]
+        Like.objects.create(user_id=user.id, article_id=articles[-1].id)
+
+        # When
+        result_articles = get_article_list(0, 10)
+        result_counts = [a.like_set.count() for a in result_articles]
+
+        with self.assertNumQueries(2):  # N+1 문제를 assertNumQuerise를 이용해서 잡을 수 있다
+            # 장고 admin과 pagenator를 사용할 때 where절 없는 sql문을 날리기에 대용량 쿼리를 날리 시에는 조심헤야한다
+            # When
+            result_articles = get_article_list(0, 10)
+            result_counts = [a.like_set.count() for a in result_articles]
+
+            # Then
+            self.assertEqual(len(result_articles), 10)
+            self.assertEqual(1, result_counts[0])
+            self.assertEqual(
+                [a.id for a in reversed(articles[10:21])],
+                [a.id for a in result_articles],
+            )
+
+        # Then
+        # 간헐적 테스트 실패 -> 비교가 목적이나 순서가 다른 경우에 문제가 생긴 -> set으로 만들어 순서를 없엔다
